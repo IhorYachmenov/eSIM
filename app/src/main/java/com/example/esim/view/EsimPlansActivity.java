19506dec13ba48e843f08d930fc3b76b9e279d4a
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-05-30 21:34:37 +0300

    Was created navigation component, background blurred image with gradient, settled default activity.

---
+++ app/src/main/java/com/example/esim/view/EsimPlansActivity.java
@@ -0,0 +1,17 @@
+package com.example.esim.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.esim.R;
+
+public class EsimPlansActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.esim_plans_main_screen);
+    }
+}
