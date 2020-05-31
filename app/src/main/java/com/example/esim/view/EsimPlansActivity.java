a393caa6b09922ba26d354e0045c6edb6c8379cf
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-05-31 03:01:40 +0300

    Created tourist cardview with payment switcher, and was added monthly and one time layouts.

--- app/src/main/java/com/example/esim/view/EsimPlansActivity.java
+++ app/src/main/java/com/example/esim/view/EsimPlansActivity.java
@@ -1,16 +1,20 @@
 package com.example.esim.view;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.databinding.DataBindingUtil;
 
@@ -20,7 +24,7 @@ import com.example.esim.viewmodel.EsimPlansViewModel;
 
 public class EsimPlansActivity extends AppCompatActivity {
 
-    // Plan Switcher
+    // Plan Switcher eSIM/Virtual Numbers
 
     TextView eSIMButton;
     TextView eSIMLabel;
@@ -30,6 +34,25 @@ public class EsimPlansActivity extends AppCompatActivity {
     LinearLayout eSimSwitcherPlan;
     ConstraintLayout eSimPlans;
     ConstraintLayout virtualNumbersPlan;
+
+    // Tourist CardView
+
+    ImageView arrowBtnTourist;
+    ImageView infoMain;
+    ConstraintLayout paymentPlan;
+    CardView cardViewTourist;
+    ConstraintLayout expandedMenuTouristMonthly;
+    ConstraintLayout expandedMenuTouristOnetime;
+
+    // Tourist payment switcher
+
+    SwitchCompat paymentSwitchTourist;
+    TextView oneTimeTextTouristPayment;
+    ImageView oneTimeImageTouristPayment;
+    TextView monthlyTextTouristPayment;
+    ImageView monthlyImageTouristPayment;
+
+
 
 
     EsimPlansViewModel viewModel = new EsimPlansViewModel();
@@ -42,6 +65,8 @@ public class EsimPlansActivity extends AppCompatActivity {
         binding.setViewModel(viewModel);
         viewModel.onCreate();
         planSwitcher();
+        expandPlanTourist();
+        planPaymentSwitcherTourist();
 
     }
 
@@ -104,6 +129,73 @@ public class EsimPlansActivity extends AppCompatActivity {
 
                     virtualNumbersPlan.setVisibility(View.GONE);
                     eSimPlans.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+
+    }
+
+    public void expandPlanTourist() {
+        arrowBtnTourist = findViewById(R.id.vector);
+        infoMain = findViewById(R.id.question);
+        paymentPlan = findViewById(R.id.payment_plan_tourist);
+        cardViewTourist = findViewById(R.id.card_view_tourist);
+        expandedMenuTouristMonthly = findViewById(R.id.expanded_menu_monthly_tourist);
+        expandedMenuTouristOnetime = findViewById(R.id.expanded_menu_onetime_tourist);
+
+        arrowBtnTourist.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                if (paymentPlan.getVisibility() == View.GONE && expandedMenuTouristMonthly.getVisibility() == View.GONE) {
+                    TransitionManager.beginDelayedTransition(cardViewTourist, new AutoTransition());
+                    paymentPlan.setVisibility(View.VISIBLE);
+                    expandedMenuTouristMonthly.setVisibility(View.VISIBLE);
+                    infoMain.setVisibility(View.VISIBLE);
+                    arrowBtnTourist.setImageResource(R.drawable.ic_vector_expand);
+                } else {
+                    TransitionManager.beginDelayedTransition(cardViewTourist, new AutoTransition());
+                    paymentPlan.setVisibility(View.GONE);
+                    expandedMenuTouristMonthly.setVisibility(View.GONE);
+                    infoMain.setVisibility(View.GONE);
+                    arrowBtnTourist.setImageResource(R.drawable.ic_vector_not);
+                }
+            }
+        });
+    }
+
+    public void planPaymentSwitcherTourist() {
+
+        paymentSwitchTourist = findViewById(R.id.switch_plan_tourist);
+        oneTimeTextTouristPayment = findViewById(R.id.one_time_tourist);
+        oneTimeImageTouristPayment = findViewById(R.id.one_time_info_tourist);
+        monthlyTextTouristPayment = findViewById(R.id.monthly_tourist);
+        monthlyImageTouristPayment = findViewById(R.id.monthly_info_tourist);
+
+        paymentSwitchTourist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (paymentSwitchTourist.isChecked()) {
+                    monthlyTextTouristPayment.setTextColor(Color.WHITE);
+                    monthlyImageTouristPayment.setColorFilter(Color.WHITE);
+
+                    oneTimeTextTouristPayment.setTextColor(Color.parseColor("#640E0D0D"));
+                    oneTimeImageTouristPayment.setColorFilter(Color.parseColor("#640E0D0D"));
+
+                    expandedMenuTouristMonthly.setVisibility(View.VISIBLE);
+                    expandedMenuTouristOnetime.setVisibility(View.GONE);
+
+                } else {
+
+                    monthlyTextTouristPayment.setTextColor(Color.parseColor("#640E0D0D"));
+                    monthlyImageTouristPayment.setColorFilter(Color.parseColor("#640E0D0D"));
+
+                    oneTimeTextTouristPayment.setTextColor(Color.WHITE);
+                    oneTimeImageTouristPayment.setColorFilter(Color.WHITE);
+
+                    expandedMenuTouristMonthly.setVisibility(View.GONE);
+                    expandedMenuTouristOnetime.setVisibility(View.VISIBLE);
 
                 }
             }
